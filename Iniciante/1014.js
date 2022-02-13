c67e978e817d51f26156af917ff010c5c3b49dbc
Lira <emmanuel.lira@capgemini.com>
2022-02-13 17:45:27 -0300

    issues upload

--- Iniciante/1014.js
+++
@@ -1,9 +0,0 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
-
-const distancia = parseInt(lines.shift());
-const totalGasto = parseFloat(lines.shift());
-
-const consumo = distancia / totalGasto;
-
-console.log(`${consumo.toFixed(3)} km/l`);
--- Iniciante/1016.js
+++
@@ -1,8 +0,0 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
-
-const distancia = parseInt(lines.shift());
-
-const tempo = distancia * 2;
-
-console.log(`${tempo} minutos`);
