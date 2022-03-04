0bfa81509c9726fcd58bbc3c9f86fa48bf1eacb9
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2022-03-03 22:18:15 -0300

    issues upload

--- Iniciante/1061.js
+++ Iniciante/1061.js
@@ -5,14 +5,14 @@ let valores1 = lines.shift().split(" ");
 const diaInicio = parseInt(valores1[1]);
 
 let valores2 = lines.shift().split(" ");
-const horaInicio = parseInt(valores2[0])
-const minutoInicio = parseInt(valores2[2])
-const segundoInicio = parseInt(valores2[4])
+const horaInicio = parseInt(valores2[0]);
+const minutoInicio = parseInt(valores2[2]);
+const segundoInicio = parseInt(valores2[4]);
 
 let valores3 = lines.shift().split(" ");
 const diaFim = parseInt(valores3[1]);
 
 let valores4 = lines.shift().split(" ");
-const horaFim = parseInt(valores4[0])
-const minutoFim = parseInt(valores4[2])
-const segundoFim = parseInt(valores4[4])
+const horaFim = parseInt(valores4[0]);
+const minutoFim = parseInt(valores4[2]);
+const segundoFim = parseInt(valores4[4]);
