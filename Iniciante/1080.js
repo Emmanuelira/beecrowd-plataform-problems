1a6728861c304be42fe5124aa03169451d866f6d
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2022-03-26 12:26:30 -0300

    issues upload

---
+++ Iniciante/1080.js
@@ -0,0 +1,22 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const valores = [];
+
+for(let i = 0; i < 100; i++) {
+    const valor = parseInt(lines.shift());
+    valores.push(valor);
+}
+
+let maiorNumero = valores[0];
+let posicaoMaior = 0;
+
+for(let indice in valores) {
+    if(valores[indice] > maiorNumero) {
+        maiorNumero = valores[indice];
+        posicaoMaior = indice + 1;
+    }
+}
+
+console.log(maiorNumero);
+console.log(posicaoMaior);
