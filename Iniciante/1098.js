46f9970f8d459a0bf29e832283026819a91ffce9
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2022-03-31 15:48:37 -0300

    issues upload

--- Iniciante/1098.js
+++ Iniciante/1098.js
@@ -8,21 +8,20 @@ while(i <= 2) {
         let letraI = i;
         let letraJ = jota;
         
-        if(i % 1 === 0) {
+        if(i.toFixed(1) % 1 === 0) {
             letraI = Math.round(i);
         } else {
             letraI = i.toFixed(1);
         }
         
-        if(jota % 1 === 0) {
+        if(jota.toFixed(1) % 1 === 0) {
             letraJ = Math.round(letraJ);
         } else {
             letraJ = jota.toFixed(1);
         }
+        
         console.log(`I=${letraI} J=${letraJ}`);
     }
     i += 0.2;
     j += 0.2;
 }
-
-// INCOMPLETO
