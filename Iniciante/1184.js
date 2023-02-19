d494e40e74657f51c05ecaea480969957a73080e
Emmanuel da Nóbrega Lira <57020710+Emmanuelira@users.noreply.github.com>
2023-02-19 12:54:22 -0300

    issues upload

---
+++ Iniciante/1184.js
@@ -0,0 +1,31 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const operation = lines.shift();
+const matrix = [];
+let finalValue = 0;
+let quantity = 0;
+
+for(let i = 0; i < 12; i++) {
+    const array = [];
+    for(let i = 0; i < 12; i++){
+        array.push(parseFloat(lines.shift()));
+    }
+    matrix.push(array);
+}
+
+let ref = 0;
+for(let i = 0; i < matrix.length; i++) {
+    const array = matrix[i];
+    for(let j = 0; j < ref; j++) {
+        finalValue += array[j];
+        quantity++;
+    }
+    ref++;
+}
+
+if(operation === 'M') {
+    finalValue /= quantity;
+}
+
+console.log(finalValue.toFixed(1));
