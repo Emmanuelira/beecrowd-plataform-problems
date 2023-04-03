e130dcece866499cb0b746ae58d66d5074187362
Emmanuel da Nóbrega Lira <57020710+Emmanuelira@users.noreply.github.com>
2023-04-03 09:14:11 -0300

    Update 2139.js

--- Iniciante/2139.js
+++ Iniciante/2139.js
@@ -4,14 +4,16 @@ var lines = input.split('\n');
 const christmas = new Date('12/25/2016');
 while(lines.length > 0) {
     const [ month, day ] = lines.shift().split(' ');
-    const today = new Date(`${month}/${day}/2016`);
-    const daysRemaining = (christmas - today) / 86400000;
-    let result = '';
+    if(month && day) {
+        const today = new Date(`${month}/${day}/2016`);
+        const daysRemaining = Math.round((christmas - today) / 86400000);
+        let result = '';
 
-    if(daysRemaining === 0) result = 'E natal!';
-    else if(daysRemaining === 1) result = 'E vespera de natal!';
-    else if(daysRemaining > 1) result = `Faltam ${daysRemaining} dias para o natal!`; 
-    else result = 'Ja passou!';
-    
-    console.log(result);
+        if(daysRemaining === 0) result = 'E natal!';
+        else if(daysRemaining === 1) result = 'E vespera de natal!';
+        else if(daysRemaining > 1) result = `Faltam ${daysRemaining} dias para o natal!`; 
+        else result = 'Ja passou!';
+        
+        console.log(result);
+    }
 }
