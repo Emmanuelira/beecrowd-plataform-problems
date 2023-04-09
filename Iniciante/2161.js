a361a0fa5f3a072d14073c67cd26e07f95629c26
Emmanuel da Nóbrega Lira <57020710+Emmanuelira@users.noreply.github.com>
2023-04-09 12:28:06 -0300

    Raiz Quadrada de 10

---
+++ Iniciante/2161.js
@@ -0,0 +1,29 @@
+/*
+Uma das formas de calcular a raiz quadrada de um número natural é pelo método das frações periódicas continuadas. Esse método usa como denominador uma repetição de frações. Essa repetição pode ser feita uma quantidade específica de vezes.
+
+Por exemplo, ao repetir 2 vezes a fração continuada para calcular a raiz quadrada de 10, temos a fórmula abaixo.
+
+
+
+Sua tarefa é, dado o número N de repetições, calcular o valor aproximado da raiz quadrada de 10.
+
+Entrada
+A entrada é um número natural N (0 ≤ N ≤ 100), que indica o número de repetições do denominador na fração continuada.
+
+Saída
+A saída é o valor aproximado da raiz quadrada com 10 casas decimais.
+*/
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const number = parseInt(lines.shift());
+
+function recursive(ref) {
+    if(ref === 1) return 1/6;
+    else return 1/(6 + recursive(--ref));
+}
+
+let result = 3;
+if(number > 0) result += recursive(number);
+console.log(result.toFixed(10));
