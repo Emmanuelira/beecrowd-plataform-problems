76eaaf4fc902f8d351dedff84c8141161d9a5b5d
Emmanuel da Nóbrega Lira <57020710+Emmanuelira@users.noreply.github.com>
2023-04-21 08:49:27 -0300

    Múltiplos

--- Iniciante/1044.js
+++ Iniciante/1044.js
@@ -1,3 +1,13 @@
+/*
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
